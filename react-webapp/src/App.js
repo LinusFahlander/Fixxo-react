7ed4ceaf57d255c8a09e31a1530bb6e7d642a04b
LinusFahlander <fahlander.linus@gmail.com>
2022-10-31 12:12:53 +0100

    2h in

--- react-webapp/src/App.js
+++ react-webapp/src/App.js
@@ -1,13 +1,33 @@
 import './style.min.css';
-import ContactsView from './views/ContactsView' ;
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import HomeView from './views/HomeView' ;
+import CategoriesView from './views/CategoriesView';
+import ProductsView from './views/ProductsView';
+import ContactsView from './views/ContactsView' ;
+import SearchView from './views/SearchView';
+import CompareView from './views/CompareView';
+import ShoppingCartView from './views/ShoppingCartView';
+import WishListView from './views/WishListView';
+
+import NotFoundView from './views/NotFoundView';
 
 function App() {
   return (
-    <>
-    <HomeView />
-    <ContactsView />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/categories" element={<CategoriesView />} />
+        <Route path="/products" element={<ProductsView />} />
+        <Route path="/contacts" element={<ContactsView />} />
+        <Route path="/search" element={<SearchView />} />
+        <Route path="/compare" element={<CompareView />} />
+        <Route path="/wishlist" element={<WishListView />} />
+        <Route path="/shoppingcart" element={<ShoppingCartView />} />
+
+        <Route path="*" element={<NotFoundView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- react-webapp/src/views/NotFoundView.js
+++ react-webapp/src/views/NotFoundView.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import MainMenuSection from '../sections/MainMenuSection'
 
-const HomeView = () => {
+const NotFoundView = () => {
   return (
     <>
       <MainMenuSection />
+      <h1>404 - Page Not Found</h1>
     </>
   )
 }
 
-export default HomeView
+export default NotFoundView
